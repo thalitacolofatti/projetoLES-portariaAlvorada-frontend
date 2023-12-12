2418529d43f0240e3d91f9345054b8021ab70dd3
Thalita Colofatti <62973671+thalitacolofatti@users.noreply.github.com>
2023-12-12 00:37:34 -0300

    update axios

--- axios.ts
+++ axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const makeRequest = axios.create({
-    baseURL: 'https://projetoles-portaria-alvorada-backend.vercel.app/',
+    baseURL: 'https://projetoles-portaria-alvorada-backend.vercel.app',
     withCredentials: true
 })
