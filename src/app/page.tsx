7cf248e5c2ce98d0320867ec3d32d6c946e224da
Thalita Colofatti <62973671+thalitacolofatti@users.noreply.github.com>
2023-12-11 21:27:32 -0300

    update page eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 'use client';
 
+import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const router = useRouter();
-  router.push('/main');
+
+  useEffect(() => {
+    router.push('/main');
+  }, [router]);
 
   return (
     <div></div>
