3bffe7128f7c6c2fce112c87ed5a31ad18a490c4
Thalita Colofatti <62973671+thalitacolofatti@users.noreply.github.com>
2023-12-13 20:41:36 -0300

    update Home router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 'use client';
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/router';
 
 export default function Home() {
   const router = useRouter();
 
-  useEffect(() => {
-    router.push('/main');
-  }, [router]);
+  router.push('/main');
 
   return (
     <div></div>
